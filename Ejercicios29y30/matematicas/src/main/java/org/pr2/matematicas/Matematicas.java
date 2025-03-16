a6ca17cd0b85dd35aa7018d00f99071a8383c9b7
PABLO MOLINA GARCÍA <pablo.molinagarcia1@usp.ceu.es>
2025-03-16 16:30:37 +0100

    Subida ejercicios tests

---
+++ Ejercicios29y30/matematicas/src/main/java/org/pr2/matematicas/Matematicas.java
@@ -0,0 +1,9 @@
+package main.java.org.pr2.matematicas;
+
+
+public class Matematicas{
+    public static int sumaNumeros(int n){
+        if (n < 0) throw new ArithmeticException("La suma hasta el n-ésimo número natural no es aplicable a un n menor que 0");
+        return n == 0?0:n + sumaNumeros(n - 1);
+    }
+}
